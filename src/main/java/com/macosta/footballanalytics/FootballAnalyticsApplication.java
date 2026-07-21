1732fbe40d8a740706c835e3a42d41ef8e3012f0
Miguel Acosta <miguel.acosta.sotodosos@gmail.com>
2026-07-21 19:39:38 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/macosta/footballanalytics/FootballAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.macosta.footballanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macosta/footballanalytics/FootballAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macosta.footballanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
